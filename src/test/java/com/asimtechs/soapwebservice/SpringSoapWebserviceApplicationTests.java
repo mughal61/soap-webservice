7ec4af6a48531652ca3f91645a1662fe7560f740
mughal61 <asim.mughal61@gmail.com>
2023-08-13 12:29:00 -0500

    Initial Comit

---
+++ src/test/java/com/asimtechs/soapwebservice/SpringSoapWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asimtechs.soapwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoapWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
